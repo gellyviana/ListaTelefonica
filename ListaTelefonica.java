bcc5ce6cd2eacd80cc150a28e7e5b2e35b60cbb0
gellyviana <gellyviana@outlook.com>
2017-04-04 17:49:33 -0300

    Alteração no main

--- ListaTelefonica.java
+++ ListaTelefonica.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class ListaTelefonica{
--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 public class Main {
 	public static void main(String[] args) {
 			Dados pessoa1 = new Dados();
-			pessoa1.setNome("Eucharles");
+			pessoa1.setNome("Júlio");
 			pessoa1.setTelefone("(84)98788-8888");
+			Dados pessoa2 = new Dados();
+			pessoa2.setNome("Antonia");
+			pessoa2.setTelefone("(84)98788-8888");
+			Dados pessoa3 = new Dados();
+			pessoa3.setNome("Marcos");
+			pessoa3.setTelefone("(84)98788-8888");
+			Dados pessoa4 = new Dados();
+			pessoa4.setNome("Gelly");
+			pessoa4.setTelefone("(84)98788-8888");
 			ListaTelefonica lista = new ListaTelefonica();
 			lista.adiciona(pessoa1.getNome(), pessoa1.getTelefone());
+			lista.adiciona(pessoa2.getNome(), pessoa2.getTelefone());
+			lista.adiciona(pessoa3.getNome(), pessoa3.getTelefone());
+			lista.adiciona(pessoa3.getNome(), pessoa3.getTelefone());
 			lista.imprimeLista();
 		}	
 }
